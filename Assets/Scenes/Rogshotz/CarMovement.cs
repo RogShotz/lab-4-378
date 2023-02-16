23ea6e82e3187cd870157b11617a5fc9e5e4dfe4
Luke Rowe <RogShotz@users.noreply.github.com>
2023-02-15 18:07:09 -0800

    Add Car Prefab, add more map stuff

---
+++ Assets/Scenes/Rogshotz/CarMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    private float speed = 0f;
+    
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform carTrigger;
+    [SerializeField] private LayerMask inCamera;
+    // Update is called once per frame
+    void Update()
+    {
+        if (InCamera())
+        {
+            Debug.Log("Car Trigger");
+            speed = -4f;
+        } else {
+            speed = 0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(speed, 0);
+    }
+    private bool InCamera()
+    {
+        return Physics2D.OverlapCircle(carTrigger.position, 0.2f, inCamera);
+    }
+}
