32dc6f7375f14ec06f8af890fe3a8ddc873a28cb
Nathan McCutchen <n8wm@icloud.com>
2023-02-15 17:32:44 -0800

    adjustments to implementation of animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,8 +11,10 @@ public class PlayerController : MonoBehaviour
     public float acceleration = 500f;
     public float jumpForce = 6f;
     public float jumpHold = 0.35f;
+    public float crouchTransTime = 0.15f;
     private float jumpTimer = 0f;
     private bool isJumping = false;
+    private bool isCrouching = false;
     private bool hasLiftedOff = false; // Used in Alex_Jump animation event
     private Rigidbody2D rb;
 
@@ -50,6 +52,20 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+        if (isCrouching) {
+            Vector2 targetColliderOffset = new Vector2(bc.offset.x, -(3f/32f));
+            Vector2 targetColliderSize = new Vector2(bc.size.x, defaultColliderSize.y - (6f/32f));
+            bc.offset = Vector2.Lerp(bc.offset, targetColliderOffset, Time.fixedDeltaTime / crouchTransTime);
+            bc.size = Vector2.Lerp(bc.size, targetColliderSize, Time.fixedDeltaTime / crouchTransTime);
+            animator.SetBool("IsCrouching", true);
+        } else {
+            isCrouching = false;
+            animator.SetBool("IsCrouching", false);
+            animator.speed = 1;
+            bc.offset = Vector2.Lerp(bc.offset, defaultColliderOffset, Time.fixedDeltaTime / crouchTransTime);
+            bc.size = Vector2.Lerp(bc.size, defaultColliderSize, Time.fixedDeltaTime / crouchTransTime);
+        }
+
         maxSpeed += Time.fixedDeltaTime * 0.1f; // Increase max speed by 1 every 10 seconds
     }
 
@@ -72,16 +88,8 @@ public class PlayerController : MonoBehaviour
 
     void OnCrouch(InputValue value)
     {
-        if (value.isPressed) {
-            bc.offset = new Vector2(bc.offset.x, bc.offset.y * -100);
-            bc.size = new Vector2(bc.size.x, bc.size.y * 0.8125f);
-            animator.SetBool("IsCrouching", true);
-        } else {
-            animator.SetBool("IsCrouching", false);
-            animator.speed = 1;
-            bc.offset = defaultColliderOffset;
-            bc.size = defaultColliderSize;
-        }
+        if (value.isPressed) isCrouching = true;
+        else isCrouching = false;
     }
 
     void LiftedOff()
@@ -92,8 +100,8 @@ public class PlayerController : MonoBehaviour
     bool IsGrounded()
     {
         return Physics2D.OverlapBox(
-            transform.position - new Vector3(0, transform.localScale.y / 6, 0),
-            new Vector2(transform.localScale.x - 0.1f, 0.1f),
+            transform.position - new Vector3(0, transform.localScale.y / 2f, 0),
+            new Vector2(transform.localScale.x / 2f - 0.1f, 0.1f),
             0,
             LayerMask.GetMask("Ground")
         );
@@ -103,8 +111,8 @@ public class PlayerController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(
-            transform.position - new Vector3(0, transform.localScale.y / 6, 0),
-            new Vector2(transform.localScale.x - 0.1f, 0.1f)
+            transform.position - new Vector3(0, transform.localScale.y / 2f, 0),
+            new Vector2(transform.localScale.x / 2f - 0.1f, 0.1f)
         );
     }
 }
