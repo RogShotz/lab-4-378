378e7fa747a055c462f8e3be849961f8eb484f13
cwmcgiffert <91047092+rcamwm@users.noreply.github.com>
2023-02-20 22:28:59 -0800

    Added basic SFX

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    public AudioSource ridingAudioSource;
+    public AudioSource landingAudioSource;
+    public AudioSource collisionAudioSource;
 
     public float maxSpeed = 5f;
     public float acceleration = 500f;
@@ -29,6 +32,9 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        ridingAudioSource = GameObject.Find("Skateboard Riding").GetComponent<AudioSource>();
+        landingAudioSource = GameObject.Find("Skateboard Landing").GetComponent<AudioSource>();
+        collisionAudioSource = GameObject.Find("Skateboard Stopping").GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody2D>();
         bc = GetComponent<BoxCollider2D>();
         defaultColliderOffset = bc.offset;
@@ -68,21 +74,51 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (!InCamera()) {
-            OnDeath();
-        }
+        InCameraUpdates(); // if (!InCamera()) {...}
 
         if (rb.velocity.x < maxSpeed + move)
             rb.AddForce(new Vector2(acceleration * Time.fixedDeltaTime, 0) * rb.mass);
         maxSpeedTarget = Mathf.Clamp(rb.velocity.x, 0, maxSpeed + move);
         rb.velocity = Vector2.Lerp(rb.velocity, new Vector2(maxSpeedTarget, rb.velocity.y), Time.fixedDeltaTime * 6f);
 
-        if (IsGrounded() && animator.speed == 0 && animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == "Alex_Jump")
-        {
-            animator.speed = 1;
-            animator.SetBool("IsJumping", false);
-        }
+        IsGroundedUpdates(); // if (IsGrounded()) {...}
+        IsJumpingUpdates(); // if (isJumping) {...}
+        IsCrouchingUpdates(); // if (isCrouching) {...}
 
+        maxSpeed += Time.fixedDeltaTime * 0.1f; // Increase max speed by 1 every 10 seconds
+    }
+
+    private void InCameraUpdates()
+    {
+        if (!InCamera()) {
+            OnDeath();
+        }
+    }
+
+    private void IsGroundedUpdates()
+    {
+        if (IsGrounded())
+        {
+            if (animator.speed == 0 && animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == "Alex_Jump")
+            {
+                animator.speed = 1;
+                animator.SetBool("IsJumping", false);
+            }
+
+            if (!ridingAudioSource.isPlaying)
+            {
+                ridingAudioSource.Play();
+                landingAudioSource.Play();
+            }
+        }
+        else
+        {
+            ridingAudioSource.Pause();
+        }
+    }
+
+    private void IsJumpingUpdates()
+    {
         // adapted from https://www.youtube.com/watch?v=j111eKN8sJw
         if (isJumping)
         {
@@ -93,7 +129,10 @@ public class PlayerController : MonoBehaviour
                 jumpTimer -= Time.fixedDeltaTime;
             }
         }
+    }
 
+    private void IsCrouchingUpdates() 
+    {
         if (isCrouching)
         {
             Vector2 targetColliderOffset = new Vector2(bc.offset.x, -(3f / 32f));
@@ -109,8 +148,6 @@ public class PlayerController : MonoBehaviour
             bc.offset = Vector2.Lerp(bc.offset, defaultColliderOffset, Time.fixedDeltaTime / crouchTransTime);
             bc.size = Vector2.Lerp(bc.size, defaultColliderSize, Time.fixedDeltaTime / crouchTransTime);
         }
-
-        maxSpeed += Time.fixedDeltaTime * 0.1f; // Increase max speed by 1 every 10 seconds
     }
 
     void HoldPose()
@@ -152,6 +189,10 @@ public class PlayerController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.layer != LayerMask.NameToLayer("Ground"))
+        {
+            collisionAudioSource.Play();
+        }
         if (collision.gameObject.layer == LayerMask.NameToLayer("Fatal"))
         {
             OnDeath();
